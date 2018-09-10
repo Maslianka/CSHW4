3646ccf9467f44014975554571e57833ab183949
Nazar <nazar2131@ukr.net>
2018-09-10 12:48:43 +0300

    Added changes

---
+++ FourthHWT/Car.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FourthHWT
+{
+    class Car
+    {
+        private string name;
+        private string color;
+        private double price;
+        public const string CompanyName= "Mercedes";
+
+        public Car() { }
+        public Car(string carName, string carColor, double carPrice)
+        {
+            this.name = carName;
+            color = carColor;
+            price = carPrice;
+        }
+
+
+        public void Input()
+        {
+            Console.Write("Enter car name:");
+            name = Console.ReadLine();
+
+            Console.Write("Enter color:");
+            color = Console.ReadLine();
+
+            Console.Write("Enter price:");
+            price = double.Parse(Console.ReadLine());
+            
+        }
+        public void Print()
+        {
+            Console.WriteLine("\nYou wrote:");
+            Console.WriteLine("Name: {0}", name);
+            Console.WriteLine("Color: {0}", color);
+            Console.WriteLine("Price: {0}", price);
+        }
+
+        public void ChangePrice(double x)
+        {
+            price = price - (price * (x / 100));
+        }
+
+        public static bool operator ==(Car a, Car b)
+        {
+            return a.name.Equals(b.name) && Math.Round(a.price, 2).Equals(Math.Round(b.price, 2));
+        }
+
+        public static bool operator !=(Car a, Car b)
+        {
+            return !a.name.Equals(b.name) && Math.Round(a.price, 2).Equals(Math.Round(b.price, 2));
+        }
+
+        public override string ToString()
+        {
+            return ("Car " + name + " " + "Color " + color + " " + "Price " + price);
+        }
+    }
+}
---
+++ FourthHWT/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FourthHWT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(Car.CompanyName);
+            List<Car> cars = new List<Car>();
+            cars.Add( new Car("s200","black",90000));
+            cars.Add(new Car("s500", "white", 190000));
+            cars.Add(new Car("SLS", "black", 5000000));
+           
+            
+
+            foreach(var s in cars)
+            {
+                s.ChangePrice(10);
+            }
+            foreach (var s in cars)
+            {
+                s.Print();
+            }
+           
+            Console.ReadKey();
+        }
+    }
+}
